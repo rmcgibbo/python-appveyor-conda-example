a4e5d785cf6d1e01c765205e097851458d334b38
Robert McGibbon <rmcgibbo@gmail.com>
2014-07-26 17:10:31 -0700

    More stuff from @ogrisel

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+
+extension_module = Extension(
+    'pyappveyordemo.extension',
+     sources=['pyappveyordemo/extension.pyx']
+)
+
+
+setup(
+    name = 'python-appveyor-demo',
+    version = '1.0',
+    description = 'This is a demo package with a compiled C extension.',
+    ext_modules = cythonize([extension_module]),
+    packages=['pyappveyordemo', 'pyappveyordemo.tests'],
+)
