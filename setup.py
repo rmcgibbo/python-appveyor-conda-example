3c553ef0e4880e9eaf11b5f4b02886414d9e0fb8
Robert McGibbon <rmcgibbo@gmail.com>
2014-07-27 02:16:20 -0700

    version string

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
+import os
 from setuptools import setup, Extension
 from Cython.Build import cythonize
+
+#####################################
+VERSION = "1.0"
+ISRELEASED = False
+__version__ = VERSION
+#####################################
+
 
 extension_module = Extension(
     'pyappveyordemo.extension',
@@ -7,9 +15,16 @@ extension_module = Extension(
 )
 
 
+if os.environ['CONDA_BUILD']:
+    with open('__conda_version__.txt', 'w') as f:
+        if ISRELEASED:
+            f.write(VERSION)
+        else:
+            f.write(VERSION + '.dev')
+
 setup(
     name = 'python-appveyor-demo',
-    version = '1.0',
+    version = VERSION,
     description = 'This is a demo package with a compiled C extension.',
     ext_modules = cythonize([extension_module]),
     zip_safe=False,
