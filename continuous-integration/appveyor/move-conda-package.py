2e7eb45feed8f990dd06234212ce4b2f35de04fb
Robert McGibbon <rmcgibbo@gmail.com>
2014-07-27 02:39:14 -0700

    version string

--- continuous-integration/appveyor/move-conda-package.py
+++
@@ -1,7 +0,0 @@
-import sys
-import subprocess
-import shutil
- 
-f = subprocess.check_output(['conda', 'build', sys.argv[1], '--output'], shell=True)
-path = [line.strip() for line in f.split('\n') if line.strip().endswith('.bz2')][0]
-shutil.move(path, '.')
