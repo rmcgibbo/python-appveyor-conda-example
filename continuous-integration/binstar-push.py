e0b8866d3b1b6a9fc896d511227523391a1501f4
Robert T. McGibbon <rmcgibbo@gmail.com>
2015-09-15 11:32:49 -0700

    Update binstar-push.py

--- continuous-integration/binstar-push.py
+++ continuous-integration/binstar-push.py
@@ -4,7 +4,7 @@ import subprocess
 import traceback
 
 token = os.environ['BINSTAR_TOKEN']
-cmd = ['anaconda_server', '-t', token, 'upload', '--force']
+cmd = ['binstar', '-t', token, 'upload', '--force']
 cmd.extend(glob.glob('*.tar.bz2'))
 try:
     subprocess.check_call(cmd)
